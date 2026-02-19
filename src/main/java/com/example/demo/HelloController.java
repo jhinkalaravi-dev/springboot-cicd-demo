84be9d63c4029cb165f1477a11d8ee6079f93f8c
Ravi Kumar <jhinkalaravi@gmail.com>
2026-02-19 18:42:25 +0530

    new change

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello CI/CD Pipeline! Ravi Kumar";
+        return "Hello CI/CD Pipeline! Ravi";
     }
 }
